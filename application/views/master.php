0cb598ba92c077139bac3f16903b4beec3401be7
jackson <jackson20000@gmail.com>
2019-08-27 11:55:00 +0530

    height change for pages

--- application/views/master.php
+++ application/views/master.php
@@ -1,18 +1,35 @@
 <style>
+    .button-custom {
+        width: 140px;
+        height: 45px;
+        font-size: 11px;
+        text-transform: uppercase;
+        letter-spacing: 2.5px;
+        font-weight: 500;
+        color: #000;
+        background-color: #fff;
+        border: none;
+        border-radius: 45px;
+        box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+        transition: all 0.3s ease 0s;
+        cursor: pointer;
+        outline: none;
+    }
+
+    .button-custom:hover {
+        background-color: gray;
+        box-shadow: 0px 5px 5px gray;
+        color: #fff;
+        transform: translateY(-7px);
+    }
     .book_section {
-        padding:14% 10%;    
+        padding:12% 10%;    
     }
     .book_section  #book-div{
         text-align:center;
-        font-size:32px;
+        font-size:25px;
     }
-    .book_section .submit_book{
-        padding:5px 10px;   
-        box-sizing: border-box;
-        border-radius: 5px; 
-        background-color:#9c9c9c;
-        color:black;
-    }
+
     @media screen and (max-width: 768px) {
     .book_section  #book-div{
        margin-bottom:16px;
@@ -21,15 +38,15 @@
   
     }
 </style>
-<section style="background-color:black;color:white;">
+<section style="background-color:black;color:white;min-height:76.9vh;">
     <div class="book_section">
         <div class="row">
             <div class="col-md-12" id="book-div">
             Master @ $150
             </div>
         </div>
-        <div style="text-align:center;padding:60px 0px;font-size:25px">
-           <a href="<?php echo base_url()?>homeweb/booking"> <button type="submit" class="submit_book">Master</button></a>
+        <div style="text-align:center;padding:50px 0px;font-size:25px">
+           <a href="<?php echo base_url()?>homeweb/booking"> <button type="submit" class="button-custom">Master</button></a>
         </div>
     </div>
 </section>
